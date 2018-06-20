38e9f744e5eb677aca24755ac05b2855ff2eddda
Daniel Owens <danielodev@gmail.com>
2018-06-20 04:36:14 -0500

    Implemented search with bugs.

--- src/EventList.js
+++ src/EventList.js
@@ -4,9 +4,38 @@ import { StyleSheet, Text, FlatList, View } from "react-native";
 import { SearchBar } from 'react-native-elements';
 import { EventItem } from "./EventItem";
 
+function hasValue( obj, search ) {
+	const cleanedTerm = search.trim().toLowerCase();
+	return Object.values( obj ).reduce( ( accumulator, current ) => {
+		const match = ( function( value ) {
+			switch( typeof value ) {
+				case "string": return value.toLowerCase().includes( cleanedTerm );
+				case "object": return value !== null ? hasValue( current, cleanedTerm ) : false;
+				default: return false;
+			}
+		} )( current );
+		return match || accumulator;
+	}, false );
+}
+
 export class EventList extends PureComponent {
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			filtered: []
+		};
+	}
 	get list() {
-		return this.props.screenProps.events;
+		return this.state.filtered.length ? this.state.filtered : this.props.screenProps.events;
+	}
+
+	search( word ) {
+		const { events } = this.props.screenProps;
+		const filtered = (word.trim() === "") ?
+			events : events.filter( event => hasValue( event, word ) );
+
+		this.setState( { filtered } );
 	}
 
 	render() {
@@ -14,6 +43,7 @@ export class EventList extends PureComponent {
 			<View>
 				<SearchBar
 					placeholder="Search"
+					onChangeText={event => this.search(event)}
 				/>
 				<FlatList
 					data={ this.list }
