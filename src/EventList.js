b38e5f63fd11a7f54d1e3db2ce850b7c42f13c4c
Daniel Owens <danielodev@gmail.com>
2018-06-20 01:16:10 -0500

    Fixed import issues. Added key values for list items.

--- src/EventList.js
+++ src/EventList.js
@@ -1,6 +1,7 @@
+import React from "react";
 import { PureComponent } from "react";
 import { StyleSheet, Text, FlatList } from "react-native";
-import { EventItem } from "./src/EventItem";
+import { EventItem } from "./EventItem";
 
 export class EventList extends PureComponent {
 	render() {
@@ -8,6 +9,7 @@ export class EventList extends PureComponent {
 			<FlatList
 				data="this.prop.events"
 				renderItem={ ({item}) => (<EventItem item={item} />) }
+				keyExtractor={ ( item, index ) => index.toString() }
 			/>
 		)
 	}
