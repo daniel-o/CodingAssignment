116606c4e95ddfa97f12b0f725776f5328313df1
Daniel Owens <danielodev@gmail.com>
2018-06-20 01:17:43 -0500

    Added EventList as displayed component.

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { EventController } from "./src/EventController";
 import { Favorites } from "./src/Favorites";
+import { EventList } from "./src/EventList";
 
 export default class App extends React.Component {
 	constructor( props ) {
@@ -27,11 +28,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+			<EventList events={ this.state.events } />
     );
   }
 }
