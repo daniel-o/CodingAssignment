2aef0cdb2403e691fe786d604d72712f90c35437
Daniel Owens <danielodev@gmail.com>
2018-06-20 03:09:23 -0500

    Added image function from EventItem. Shows Image now.

--- src/EventDetails.js
+++ src/EventDetails.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { PureComponent } from "react";
+import { Image, StyleSheet, View } from "react-native";
 import { Text } from 'react-native-elements'
 
 export class EventDetails extends PureComponent {
@@ -9,9 +10,25 @@ export class EventDetails extends PureComponent {
 		this.state = props.navigation.getParam( "item" );
 	}
 
+	imageURL() {
+		const main = this.state.performers.find( performer => performer.primary );
+		return main.image;
+	}
+
 	render() {
 		return (
-			<Text h4>{ this.state.title }</Text>
+			<View style={{ flexDirection: "column" }}>
+				<Text h4>{ this.state.title }</Text>
+				<Image source={{ uri: this.imageURL()}} style={ styles.image } />
+			</View>
 		)
 	}
 }
+
+const styles = StyleSheet.create( {
+	image: {
+		width: "85%",
+		aspectRatio: 1,
+		alignSelf: "center"
+	}
+} );
