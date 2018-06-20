49b43d75d267043da8bd2a5be2314b966ed6e4ec
Daniel Owens <danielodev@gmail.com>
2018-06-20 01:07:38 -0500

    Added EventItem to EventList.

--- src/EventList.js
+++ src/EventList.js
@@ -1,10 +1,14 @@
 import { PureComponent } from "react";
 import { StyleSheet, Text, FlatList } from "react-native";
+import { EventItem } from "./src/EventItem";
 
 export class EventList extends PureComponent {
 	render() {
 		return (
-			<FlatList data="this.prop.events" />
+			<FlatList
+				data="this.prop.events"
+				renderItem={ ({item}) => (<EventItem item={item} />) }
+			/>
 		)
 	}
 }
