3d777524837f92b4459637793e83a5f65299b155
Daniel Owens <danielodev@gmail.com>
2018-06-20 01:33:26 -0500

    Seperated data loading so it can be passed to the EventList.

--- App.js
+++ App.js
@@ -8,22 +8,36 @@ export default class App extends React.Component {
 	constructor( props ) {
 		super( props );
 
-		this.state = { events: [] };
+		this.state = {
+			page: 1,
+			events: []
+		};
 
 		this.favorites = new Favorites();
 
 		this.grabber = new EventController();
-		this.grabber.grabEvents().then( events => this.setState( { events: this.addEvents( events ) } ) );
+		this.grabNextEventPage();
 	}
 
 	/**
 	 * This method will attach a 'favorite' property to every event object loaded
 	 * from SeatGeek.
+	 * Returns a new array of events that concats from existing entries and recently loaded
+	 * to adhere to React's immutable state model.
 	 */
 	addEvents( additions ) {
-		return additions.map(
+		return this.state.events.concat( additions.map(
 			event => Object.assign( event, { favorite: this.favorites.includes( event.id ) } )
-		);
+		) );
+	}
+
+	grabNextEventPage() {
+		this.grabber.grabEvents( this.state.page ).then(
+			events => this.setState( prev => ( {
+				page: prev.page + 1,
+				events: this.addEvents( events )
+			} )
+		) );
 	}
 
   render() {
