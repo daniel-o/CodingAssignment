8035ebb577a241a200a8b02814cabadd5b14a72e
Daniel Owens <danielodev@gmail.com>
2018-06-20 02:00:04 -0500

    Created getter for finding image url.

--- src/EventItem.js
+++ src/EventItem.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { PureComponent } from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Picture } from "./Picture";
 
 export class EventItem extends PureComponent {
+	get imageURL() {
+		const main = this.props.item.performers.find( performer => performer.primary );
+		return main.image;
+	}
+
 	render() {
 		return (
-			<TouchableOpacity>
+			<TouchableOpacity style={ flexDirection: "row" }>
+				<Picture source={ this.imageURL } />
 				<Text>{ this.props.item.title }</Text>
 			</TouchableOpacity>
 		)
