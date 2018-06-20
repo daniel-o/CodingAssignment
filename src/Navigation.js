f9dfd11ec9c26a2df35146cd9fa365bf101e5304
Daniel Owens <danielodev@gmail.com>
2018-06-20 02:54:31 -0500

    Added Details screen to navigation.

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { createStackNavigator } from 'react-navigation';
 import { EventList } from "./EventList";
+import { EventDetails } from "./EventDetails";
 
 export const Navigation = createStackNavigator( {
 	Home: {
 		screen: EventList,
 		navigationOptions: {
 			header: null
+		}
+	},
+	Details: {
+		screen: EventDetails,
+		navigationOptions: {
+			title: "Event Details"
 		}
 	}
 },
