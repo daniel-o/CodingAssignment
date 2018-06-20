651490d91c9653b7afa2abcfb781374cb2e45530
Daniel Owens <danielodev@gmail.com>
2018-06-20 02:43:03 -0500

    Changed EventList to reflect props change through screenProps.

--- src/EventList.js
+++ src/EventList.js
@@ -4,13 +4,17 @@ import { StyleSheet, Text, FlatList } from "react-native";
 import { EventItem } from "./EventItem";
 
 export class EventList extends PureComponent {
+	get list() {
+		return this.props.screenProps.events;
+	}
+
 	render() {
 		return (
 			<FlatList
-				data={ this.props.events }
+				data={ this.list }
 				renderItem={ ({item}) => (<EventItem item={item} />) }
 				keyExtractor={ ( item, index ) => index.toString() }
-				onEndReached={event => this.props.nextPage()}
+				onEndReached={event => this.props.screenProps.nextPage()}
 			/>
 		)
 	}
