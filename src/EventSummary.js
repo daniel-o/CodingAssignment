8a3bf9b6d11ba3873e49be150251d9562af12dd9
Daniel Owens <danielodev@gmail.com>
2018-06-20 02:20:55 -0500

    Setup format getters for properties and display them now.

--- src/EventSummary.js
+++ src/EventSummary.js
@@ -3,10 +3,27 @@ import { PureComponent } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export class EventSummary extends PureComponent {
+	get venueLabel() {
+		const { name, city, state } = this.props.item.venue;
+		return `${ name } in ${ city }, ${ state }`
+	}
+
+	get timestamp() {
+		const date = new Date( Date.parse( this.props.item.datetime_local ) );
+		const time = date.toLocaleString( "en-US", {
+			hour: "numeric",
+			minute: "numeric",
+			hour12: true
+		} );
+		return date.toDateString() + " " + time;
+	}
+
 	render() {
 		return (
 			<View style={ styles.summary }>
 				<Text style={ styles.title }>{ this.props.item.short_title }</Text>
+				<Text>{ this.venueLabel }</Text>
+				<Text>{ this.timestamp }</Text>
 			</View>
 		)
 	}
