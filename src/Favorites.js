aadd140540148a485be499ca995db036da7c26b8
Daniel Owens <danielodev@gmail.com>
2018-06-20 03:46:02 -0500

    Added ability to store favorites in local storage when updated.

--- src/Favorites.js
+++ src/Favorites.js
@@ -1,3 +1,4 @@
+import { AsyncStorage } from "react-native";
 /**
  * This class will act as a source of truth for our list of event ids that have been
  * marked as favorites.
@@ -9,16 +10,26 @@ export class Favorites {
 
 	add( id ) {
 		this.favorites.push( id );
+		this.persist();
 		return true;
 	}
 
 	remove( id ) {
 		const index = this.favorites.findIndex( eventid => eventid === id );
 		this.favorites.splice( index, 1 );
+		this.persist();
 		return false;
 	}
 
 	includes( id ) {
 		return this.favorites.includes( id );
+	}
+
+	persist() {
+		try {
+			AsyncStorage.setItem( "favorites", JSON.stringify( this.favorites ) );
+		} catch( error ) {
+			console.error( error );
+		}
 	}
 }
