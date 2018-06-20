74a3cdf0b4b5c7a7d2273d2ea43bec4762ee54ab
Daniel Owens <danielodev@gmail.com>
2018-06-20 01:01:05 -0500

    Created tappable component with event title to display.

--- src/EventItem.js
+++ src/EventItem.js
@@ -1,5 +1,12 @@
 import { PureComponent } from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text TouchableOpacity } from "react-native";
 
 class EventItem extends PureComponent {
+	render() {
+		return (
+			<TouchableOpacity>
+				<Text>{ this.props.title }</Text>
+			</TouchableOpacity>
+		)
+	}
 }
