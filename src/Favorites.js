6b5fe5836c8d903f93bf9bf6c2369338eacb90c4
Daniel Owens <danielodev@gmail.com>
2018-06-20 00:24:09 -0500

    Started a Class that will manage a list of event ids that have been   marked as favorite.

---
+++ src/Favorites.js
@@ -0,0 +1,9 @@
+/**
+ * This class will act as a source of truth for our list of event ids that have been
+ * marked as favorites.
+ */
+export class Favorites {
+	constructor() {
+		this.favorites = new Array();
+	}
+}
