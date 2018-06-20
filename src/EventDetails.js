1a7b6cae3b77e165cd7de8b63845c040d2c47359
Daniel Owens <danielodev@gmail.com>
2018-06-20 03:01:39 -0500

    Will now assign the item params passed durning navigation to state.

--- src/EventDetails.js
+++ src/EventDetails.js
@@ -3,4 +3,15 @@ import { PureComponent } from "react";
 import { Text } from 'react-native-elements'
 
 export class EventDetails extends PureComponent {
+	constructor( props ) {
+		super( props );
+
+		this.state = props.navigation.getParam( "item" );
+	}
+
+	render() {
+		return (
+			<Text h4>{ this.state.title }</Text>
+		)
+	}
 }
