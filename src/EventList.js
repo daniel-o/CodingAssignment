3e44e0ad3ad551e89ef3442f7ac35ed9a4954de6
Daniel Owens <danielodev@gmail.com>
2018-06-20 00:52:14 -0500

    Renders a list of events passed from props now.

--- src/EventList.js
+++ src/EventList.js
@@ -2,4 +2,9 @@ import { PureComponent } from "react";
 import { StyleSheet, Text, FlatList } from "react-native";
 
 export class EventList {
+	render() {
+		return (
+			<FlatList data="this.prop.events" />
+		)
+	}
 }
