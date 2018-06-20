5da6ccb22797d3c82e56a10c45287e56dd04930c
Daniel Owens <danielodev@gmail.com>
2018-06-20 01:24:05 -0500

    Introduced pagination request to the SeatGeek API.

--- src/EventController.js
+++ src/EventController.js
@@ -5,9 +5,10 @@ export class EventController {
 			.join( "&" );
 	}
 
-	grabEvents() {
+	grabEvents( page = 1 ) {
 		const host = "https://api.seatgeek.com/2/events/"
 		const params = this.buildQueryParams( {
+			page,
 			"client_id": "MTE5NzM5MDB8MTUyOTI5Mjg5Ni42Mw",
 			"client_secret": "e4cdee90a1edb47e6de72569bc173e5d4d845dc7052232f1fde6dd3bced9691d"
 		} );
