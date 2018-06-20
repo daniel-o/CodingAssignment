34a40dd5a86ca5102a92d38c51dbd80197a6cdc3
Daniel Owens <danielodev@gmail.com>
2018-06-20 00:53:19 -0500

    Forgot to extend class from PureComponent.

--- src/EventList.js
+++ src/EventList.js
@@ -1,7 +1,7 @@
 import { PureComponent } from "react";
 import { StyleSheet, Text, FlatList } from "react-native";
 
-export class EventList {
+export class EventList extends PureComponent {
 	render() {
 		return (
 			<FlatList data="this.prop.events" />
