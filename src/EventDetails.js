4553150329cafcac71d01b0ce1d655e9bdbcee6d
Daniel Owens <danielodev@gmail.com>
2018-06-20 03:29:18 -0500

    Accepts toggleFavorite as a screenProp and attaches it to a toggle button.

--- src/EventDetails.js
+++ src/EventDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { PureComponent } from "react";
 import { Image, StyleSheet, View } from "react-native";
-import { Text } from 'react-native-elements'
+import { Text, CheckBox, Icon } from 'react-native-elements'
 
 export class EventDetails extends PureComponent {
 	constructor( props ) {
@@ -15,11 +15,27 @@ export class EventDetails extends PureComponent {
 		return main.image;
 	}
 
+	toggleFavorite() {
+		// Hack: Passing objects via react-navigation params is "pass by value"
+		this.setState( prev => ({ favorite: !prev.favorite }) );
+		this.props.screenProps.toggleFavorite( this.state.id );
+	}
+
 	render() {
 		return (
 			<View style={{ flexDirection: "column" }}>
 				<Text h4>{ this.state.title }</Text>
 				<Image source={{ uri: this.imageURL()}} style={ styles.image } />
+				<CheckBox
+					center
+					checkedIcon="heart"
+					uncheckedIcon="heart"
+					checkedColor="red"
+					uncheckedColor="black"
+					checked={ this.state.favorite }
+					onPress={ () => this.toggleFavorite() }
+					title="FAVORITE"
+				/>
 			</View>
 		)
 	}
