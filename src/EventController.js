ced553799aceacdafe16c1dea6176bca5fd45e88
Daniel Owens <danielodev@gmail.com>
2018-06-20 00:11:33 -0500

    Created method for sending simple GET request to SeatGeek.

--- src/EventController.js
+++ src/EventController.js
@@ -1,6 +1,19 @@
+export class EventController {
 	buildQueryParams( obj ) {
 		return "?" + Object.entries( obj )
 			.map( field => field.join( "=" ) )
 			.join( "&" );
+	}
+
+	grabEvents() {
+		const host = "https://api.seatgeek.com/2/events/"
+		const params = this.buildQueryParams( {
+			"client_id": "MTE5NzM5MDB8MTUyOTI5Mjg5Ni42Mw",
+			"client_secret": "e4cdee90a1edb47e6de72569bc173e5d4d845dc7052232f1fde6dd3bced9691d"
+		} );
+
+		return fetch( host + params )
+			.then( response => response.json()
+				.then( data => data.events ) );
 	}
 }
